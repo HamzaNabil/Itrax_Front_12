385dafffb8e3979956e33e64c34cf0b611953325
HamzaNabil <hamzanabil913@gmail.com>
2021-11-28 23:55:44 +0200

    add session 7

---
+++ Session7/README.md
@@ -0,0 +1,23 @@
+# JavaScript Part 1
+
+# Content : Edabit | codewars | hackerrank
+
+- Introduction : الهويه
+
+- Where To Write
+
+- Output [ document write | getElement | console log]
+
+- Statement
+
+- Comments
+
+- Variable
+
+- Operators
+
+- Decrement | Increment
+
+- Task: Simple Todo app
+
+- Function => next Lec
---
+++ Session7/js/main.js
@@ -0,0 +1,62 @@
+// Output
+
+// document.write("Test Here!");
+
+// document.getElementById("test").innerHTML = "hamada";
+
+// 200
+
+// 100
+
+// console.log("test"); // test code || divide and conqure
+
+// 100
+
+// 50
+
+// console.log("ajsgdkjasgd");
+
+// 50
+
+// get Button then click on it => get p and write inner = ay7aga
+
+var btn = document.getElementById("btn"); // statement
+
+btn.onclick = function () {
+  btn.innerHTML = "ay 7aga";
+};
+
+// Variables || DataTypes
+var num = 10; // number
+var txt = "js"; // string
+var x = false; // boolean
+
+// if condition    && all condition passed
+//  || any condition passed
+if (num == 10) {
+  console.log("yes 10");
+} else if (num == 12) {
+  console.log("yes 12");
+} else {
+  console.log("no");
+}
+
+// increment زيادة
+// decrement نقصان
+
+var code = 0;
+code++; // 1
+code++; // 2
+++code; // 3
+console.log(code); // 3
+console.log(code++); // 3
+// 4
+console.log(code); // 4
+code--; // 3
+code++; // 4
+--code; // 3
+
+console.log(--code); // 2
+console.log(code--); // 2
+// 1
+console.log(code); // 1
